e10af661db3e10ec0a360935421b374ee2e5d8e6
Gaurav Ghodinde <gauravghodinde@gmail.com>
2024-08-25 22:42:15 +0530

    final push

--- app/playground/page.jsx
+++ app/playground/page.jsx
@@ -1,24 +1,26 @@
-'use client';
-import { useEffect, useRef, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import Mapp from '../map/Map';
-import Profile from '../profile/Profile';
-import { io } from 'socket.io-client';
-import './playground.scss';
-import CanvasBg from '../canvasBg/CanvasBg';
-import { Suspense } from 'react';
-import { useUserContext } from '../context/UserContext';
-import Image from 'next/image';
-
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Mapp from "../map/Map";
+import Profile from "../profile/Profile";
+import { io } from "socket.io-client";
+import "./playground.scss";
+import CanvasBg from "../canvasBg/CanvasBg";
+import { Suspense } from "react";
+import { useUserContext } from "../context/UserContext";
+import Image from "next/image";
+import tableimg from "../../assets/table.png";
 
 function Page() {
-  var pushToTalk = false
-  const [curTable , setCurTable] = useState(-1);
+  var pushToTalk = false;
+  // const [pu,setpushToTalk] = false
+
+  const [curTable, setCurTable] = useState(-1);
   const cur = useRef();
   const outer = useRef();
   const { user, setUser, joinedUsers, setJoinedUsers } = useUserContext();
-
-  const roomId = '123456';
+  const [table, setTable] = useState(null);
+  const roomId = "123456";
   const router = useRouter();
 
   const [socket, setSocket] = useState(null);
@@ -33,35 +35,34 @@ function Page() {
   const curImg = user?.currentimage || localUserRef.current?.currentimage;
 
   useEffect(() => {
-    localUserRef.current = localStorage.getItem('userD');
+    localUserRef.current = localStorage.getItem("userD");
     if (localUserRef.current) {
       const parsedUser = JSON.parse(localUserRef.current);
       setUser(parsedUser);
     } else {
       if (!user) {
-        router.push('/');
+        router.push("/");
       }
     }
   }, [router]);
-
 
   const allMouseMove = (e) => {
     let childElementMap = e.mapParent.querySelector(`#${e.username}-map`);
     // cursors.set(e.username, e.cursorPos);
     if (childElementMap) {
       // If the child exists, apply the styles to it
-      childElementMap.style.top = `${(e.cursorPos.y - 40) * 8 / 100}px`;
-      childElementMap.style.left = `${(e.cursorPos.x - 17) * 8 / 100}px`;
+      childElementMap.style.top = `${((e.cursorPos.y - 40) * 8) / 100}px`;
+      childElementMap.style.left = `${((e.cursorPos.x - 17) * 8) / 100}px`;
     } else {
       // If the child doesn't exist, create a new element
-      childElementMap = document.createElement('img');
+      childElementMap = document.createElement("img");
       childElementMap.id = `${e.username}-map`;
-      childElementMap.className = 'otherCursorMap';
+      childElementMap.className = "otherCursorMap";
       childElementMap.src = curImg;
 
       // Apply the styles to the new element
-      childElementMap.style.top = `${(e.cursorPos.y - 40) * 8 / 100}px`;
-      childElementMap.style.left = `${(e.cursorPos.x - 17) * 8 / 100}px`;
+      childElementMap.style.top = `${((e.cursorPos.y - 40) * 8) / 100}px`;
+      childElementMap.style.left = `${((e.cursorPos.x - 17) * 8) / 100}px`;
 
       // Append the new element to the parent element
       e.mapParent.appendChild(childElementMap);
@@ -74,10 +75,10 @@ function Page() {
       childElement.style.left = `${e.cursorPos.x - 17}px`;
     } else {
       // If the child doesn't exist, create a new element
-      childElement = document.createElement('div');
+      childElement = document.createElement("div");
       childElement.id = e.username;
-      childElement.className = 'otherCursor';
-      setJoinedUsers(prev => [...prev, e.username]);
+      childElement.className = "otherCursor";
+      setJoinedUsers((prev) => [...prev, e.username]);
 
       // Apply the styles to the new element
       childElement.style.top = `${e.cursorPos.y - 40}px`;
@@ -86,50 +87,56 @@ function Page() {
       // Append the new element to the parent element
       allMouse.current.appendChild(childElement);
     }
-  }
+  };
   useEffect(() => {
     // const socketInstance = io('http://192.168.127.96:5000');
     // const socketInstance = io('http://103.209.145.248:3000');
-    const socketInstance = io('http://172.70.101.255:3000');
+    // const socketInstance = io("http://172.70.101.255:3000");
+    const socketInstance = io("http://localhost:3000");
     // const socketInstance = io('http://192.168.18.96:5000');
     // const socketInstance = io('http://172.70.100.243:5000');
 
-
-    const mapParent = document.querySelector('#allCursPos');
+    const mapParent = document.querySelector("#allCursPos");
     setSocket(socketInstance);
 
-    socketInstance.on('connect', () => {
-      socketInstance.emit('join-room', { roomId, username });
+    socketInstance.on("connect", () => {
+      socketInstance.emit("join-room", { roomId, username });
     });
 
-
-    socketInstance.on('user-left', (username) => {
+    socketInstance.on("user-left", (username) => {
       console.log(`${username} left the room`);
-      setClients(prev => prev.filter(user => user !== username));
-      setCursorPositions(prevPositions => prevPositions.filter(item => item.username !== username));
-      socketInstance.emit('connected-users', clients);
+      setClients((prev) => prev.filter((user) => user !== username));
+      // setCursorPositions(prevPositions => prevPositions.filter(item => item.username !== username));
+      socketInstance.emit("connected-users", clients);
     });
-    socketInstance.on('user-joined', (username) => {
+    socketInstance.on("user-joined", (username) => {
       console.log(`${username} joined the room`);
       // setClients(prev => prev.filter(user => user !== username));
       // setCursorPositions(prevPositions => prevPositions.filter(item => item.username !== username));
-      socketInstance.emit('connected-users', clients);
+      socketInstance.emit("connected-users", clients);
     });
 
-    socketInstance.on('remote-cursor-move', ({ username, cursorPos }) => {
+    socketInstance.on("remote-cursor-move", ({ username, cursorPos }) => {
       // console.log(`Received cursor position for ${username}:`, cursorPos);
       allMouseMove({ username, cursorPos, mapParent });
     });
 
-    socketInstance.on('connected-users-table' , (table) =>{
-      console.log(table)
-    })
+    socketInstance.on("connected-users-table", (table) => {
+      setTable(table);
+      console.log(table);
+      if (table.hasOwnProperty("1")) {
+        let lengthOfArray1 = table["1"].length;
+        console.log(lengthOfArray1); 
+      } else {
+        console.log("Key '1' does not exist.");
+      }
+    });
 
     return () => {
-      socketInstance.off('user-joined');
-      socketInstance.off('connected-users');
-      socketInstance.off('user-left');
-      socketInstance.off('remote-cursor-move');
+      socketInstance.off("user-joined");
+      socketInstance.off("connected-users");
+      socketInstance.off("user-left");
+      socketInstance.off("remote-cursor-move");
       socketInstance.disconnect();
     };
   }, [roomId, username]);
@@ -138,22 +145,28 @@ function Page() {
     if (!socket) return;
 
     const handleMouseMove = (event) => {
-      let htmlElem = document.querySelector('html');
-      myCursor.x = event.clientX, myCursor.y = event.clientY;
-      const cursorPos = { x: (htmlElem.scrollLeft + event.clientX), y: (htmlElem.scrollTop + event.clientY) };
-      socket.emit('cursor-move', { roomId, username, cursorPos });
+      let htmlElem = document.querySelector("html");
+      (myCursor.x = event.clientX), (myCursor.y = event.clientY);
+      const cursorPos = {
+        x: htmlElem.scrollLeft + event.clientX,
+        y: htmlElem.scrollTop + event.clientY,
+      };
+      socket.emit("cursor-move", { roomId, username, cursorPos });
     };
     const emitOnScroll = (e) => {
-      const cursorPos = { x: (e.target.scrollingElement.scrollLeft + myCursor.x), y: (e.target.scrollingElement.scrollTop + myCursor.y) };
-      socket.emit('cursor-move', { roomId, username, cursorPos });
-    }
+      const cursorPos = {
+        x: e.target.scrollingElement.scrollLeft + myCursor.x,
+        y: e.target.scrollingElement.scrollTop + myCursor.y,
+      };
+      socket.emit("cursor-move", { roomId, username, cursorPos });
+    };
 
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('scroll', emitOnScroll);
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("scroll", emitOnScroll);
 
     return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('scroll', handleMouseMove);
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("scroll", handleMouseMove);
     };
   }, [socket, roomId, username]);
 
@@ -163,8 +176,8 @@ function Page() {
 
   const leaveRoom = () => {
     if (!socket) return;
-    socket.emit('leave-room', { roomId, username });
-    router.push('/');
+    socket.emit("leave-room", { roomId, username });
+    router.push("/");
   };
 
   const copyRoomId = () => {
@@ -172,122 +185,120 @@ function Page() {
   };
 
   useEffect(() => {
-    document.addEventListener('contextmenu', (event) => {
+    document.addEventListener("contextmenu", (event) => {
       event.preventDefault();
     });
   }, []);
 
-
   if (!localUserRef.current && !user) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
-
-
-  const handleJoinTable = (tableId)=> {
-    console.log(curTable)
-    if(curTable!= -1){
-      handleLeaveTable(curTable);
-
+  const handleJoinTable = (tableId) => {
+    console.log(curTable);
+    if (curTable != -1) {
+      // handleLeaveTable(curTable);
       setCurTable(tableId);
+      console.log("koining table" , tableId);
       console.log(tableId);
+      // console.log(curTable);
+      handletableclick(tableId);
+    } else {
+      console.log(tableId);
+      setCurTable(tableId);
       console.log(curTable);
       handletableclick(tableId);
-    }else{
-      console.log(tableId);
-      setCurTable(tableId);
-      console.log(curTable)
-      handletableclick(tableId);
     }
-  }
+  };
   const handletableclick = (tableId) => {
+    console.log("clicked table ", tableId);
 
-    // const socketInstance = io('http://localhost:3000');
-
-    console.log("clicked table " , tableId)
-    
-      socket.emit('join-table', {tableId,username});
-      navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    socket.emit("join-table", { tableId, username });
+    console.log("joining table")
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: false })
       .then((stream) => {
         // console.log(stream)
         var madiaRecorder = new MediaRecorder(stream);
         var audioChunks = [];
-        
+
         madiaRecorder.addEventListener("dataavailable", function (event) {
-          if(pushToTalk){
+          if (pushToTalk) {
             console.log(pushToTalk);
             audioChunks.push(event.data);
-                  }
-                  // console.log(audioChunks);
-                    
-                });
-        
-                madiaRecorder.addEventListener("stop", function () {
-                    var audioBlob = new Blob(audioChunks);
-                    audioChunks = [];
-                    var fileReader = new FileReader();
-                    fileReader.readAsDataURL(audioBlob);
-                    fileReader.onloadend = function () {
-                        var base64String = fileReader.result;
-                        if(pushToTalk){
+          }
+          // console.log(audioChunks);
+        });
 
-                          console.log("emitung");
-                          socket.emit("audioStream", {base64String,tableId });
-                        }
-                    };
-        
-                    madiaRecorder.start();
-                    setTimeout(function () {
-                        madiaRecorder.stop();
-                    }, 1000);
-                });
-                madiaRecorder.start();
-                setTimeout(function () {
-                    madiaRecorder.stop();
-                }, 1000);
-            })
-            .catch((error) => {
-                console.error('Error capturing audio.', error);
-            });
-    socket.on('audioStream', (audioData) => {
-      console.log("tab",audioData.tableId);
+        madiaRecorder.addEventListener("stop", function () {
+          var audioBlob = new Blob(audioChunks);
+          audioChunks = [];
+          var fileReader = new FileReader();
+          fileReader.readAsDataURL(audioBlob);
+          fileReader.onloadend = function () {
+            var base64String = fileReader.result;
+            if (pushToTalk) {
+              console.log("emitung");
+              socket.emit("audioStream", { base64String, tableId ,username });
+              console.log("emited") 
+            }
+          };
+
+          madiaRecorder.start();
+          setTimeout(function () {
+            madiaRecorder.stop();
+          }, 1000);
+        });
+        madiaRecorder.start();
+        setTimeout(function () {
+          madiaRecorder.stop();
+        }, 1000);
+      })
+      .catch((error) => {
+        console.error("Error capturing audio.", error);
+      });
+    socket.on("audioStream", (audioData) => {
+      console.log("tab", audioData.tableId);
       console.log(audioData.base64String);
+      console.log(audioData.username);
+      console.log(audioData);
 
-      audioData = audioData.base64String
-        var newData = audioData.split(";");
-        console.log(newData);
-        newData[0] = "data:audio/ogg;";
-        newData = newData[0] + newData[1];
-        
-        var audio = new Audio(newData);
-        console.log(audio);
-        if (!audio || document.hidden || audio.src == "data:audio/ogg;base64," ){
-          return;
-        }
-        
-        audio.play();
-      
-  });
+      if(audioData.username == username){
+        return;
+      }
+      audioData = audioData.base64String;
+      var newData = audioData.split(";");
+      console.log(newData);
+      newData[0] = "data:audio/ogg;";
+      newData = newData[0] + newData[1];
 
-  }
+      var audio = new Audio(newData);
+      console.log(audio);
+      if (!audio || document.hidden || audio.src == "data:audio/ogg;base64,") {
+        return;
+      }
 
-  const handleLeaveTable = (tableId) =>{
-    socket.emit('leave-table', {tableId,username});
-  } 
+      audio.play();
+    });
+  };
 
-
-  
-
-  
+  const handleLeaveTable = (tableId) => {
+    console.log("leaving table " , tableId);
+    socket.emit("leave-table", { tableId, username });
+  };
 
   return (
-
-    <div className="playground"
-      onMouseEnter={() => (cur.current.style.display = 'block')}
-      onMouseLeave={() => (cur.current.style.display = 'none')}
-      onMouseMove={(e) => (cur.current.style.transform = `translate(${e.clientX - 17}px, ${e.clientY - 40}px)`)}
-      >
-      {(user || localUserRef.current) &&
+    <div
+      className="playground"
+      onMouseEnter={() => (cur.current.style.display = "block")}
+      onMouseLeave={() => (cur.current.style.display = "none")}
+      onMouseMove={(e) =>
+        (cur.current.style.transform = `translate(${e.clientX - 17}px, ${
+          e.clientY - 40
+        }px)`)
+      }
+    >
+      {(user || localUserRef.current) && (
         <>
           <CanvasBg />
           <Image
@@ -298,31 +309,60 @@ function Page() {
             height={100}
             ref={cur}
           />
-          <div
-            className="outer"
-            ref={outer}
-
-          >
+          <div className="outer" ref={outer}>
             <div className="front">
               <Profile user={user || localUserRef.current} />
               <div className="mapCover">
                 <Mapp />
-                <button onClick={()=>handleJoinTable(1)}>table 1</button>
-              <button onClick={()=>handleJoinTable(2)}>table 2</button>
-              <button onClick={()=>handleJoinTable(3)}>table 3</button>
-              <button onClick={()=>handleLeaveTable()}>leave</button>
 
-              {/* <button onClick={()=>handleLeaveTable()}>table 3</button> */}
-              <button onClick={()=>{
-                pushToTalk = !pushToTalk;
-                console.log(pushToTalk);
-              }}>talk</button>
+                {/* <button onClick={()=>handleLeaveTable()}>table 3</button> */}
+                <button
+                  onClick={() => {
+                    pushToTalk = !pushToTalk;
+                    console.log(pushToTalk);
+                  }}
+                >
+                  talk
+                </button>
               </div>
             </div>
           </div>
-          <div className="allCursors" ref={allMouse}></div>
+          <div className="allCursors" ref={allMouse}>
+            <div className="table">
+              <div className="number">{table && table.hasOwnProperty("1") && table["1"].length} </div> 
+              <Image src={tableimg} onClick={() => handleJoinTable(1)} />
+              <button
+              className="buttonleave"
+              onClick={() => handleLeaveTable(1)}
+            >
+              leave table
+            </button>
+            </div>
+            <div className="table">
+              <div className="number">{table && table.hasOwnProperty("2") && table["2"].length} </div> 
+              <Image src={tableimg} onClick={() => handleJoinTable(2)} />
+              <button
+              className="buttonleave"
+              onClick={() => handleLeaveTable(2)}
+            >
+              leave table
+            </button>
+            </div>
+            <div className="table">
+              <div className="number">{table && table.hasOwnProperty("3") && table["3"].length} </div> 
+              <Image src={tableimg} onClick={() => handleJoinTable(3)} />
+              <button
+              className="buttonleave"
+              onClick={() => handleLeaveTable(3)}
+            >
+              leave table
+            </button>
+            </div>
+          
+            
+          </div>
         </>
-      }
+      )}
     </div>
   );
 }

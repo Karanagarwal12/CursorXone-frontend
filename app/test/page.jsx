77eb2ff6b7a60666b8c9199c387c12e50cc0b159
Gaurav Ghodinde <gauravghodinde@gmail.com>
2024-08-26 01:16:22 +0530

    added emoji selector and animation code

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,89 +1,91 @@
 'use client';
 
 
+import {EmojiSelector} from '@/components/emojiSelector';
 import { Suspense, useEffect, useState } from 'react';
 
+import './test.scss';
 import { io } from 'socket.io-client';
 
 function Page() {
 
     // const [push,setpush] = useState(false)
-    var push = false
-    const openRoom = ()=> {
+    // var push = false
+    // const openRoom = ()=> {
 
-    }
-    const joinRoom = ()=> {
+    // }
+    // const joinRoom = ()=> {
 
-    }
+    // }
 
 
-    useEffect(()=>{
-        let socket = io("http://172.70.101.255:3000", {
-            transports: ['websocket'],
-            upgrade: false
-        });
-        socket.on('connect', () => {
-            navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-              .then((stream) => {
-                  console.log(stream)
-                  var madiaRecorder = new MediaRecorder(stream);
-                  var audioChunks = [];
+    // useEffect(()=>{
+    //     let socket = io("http://172.70.101.255:3000", {
+    //         transports: ['websocket'],
+    //         upgrade: false
+    //     });
+    //     socket.on('connect', () => {
+    //         navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    //           .then((stream) => {
+    //               console.log(stream)
+    //               var madiaRecorder = new MediaRecorder(stream);
+    //               var audioChunks = [];
           
-                  madiaRecorder.addEventListener("dataavailable", function (event) {
-                    if(push){
-                      console.log(push);
-                      audioChunks.push(event.data);
-                    }
+    //               madiaRecorder.addEventListener("dataavailable", function (event) {
+    //                 if(push){
+    //                   console.log(push);
+    //                   audioChunks.push(event.data);
+    //                 }
                       
-                  });
+    //               });
           
-                  madiaRecorder.addEventListener("stop", function () {
-                      var audioBlob = new Blob(audioChunks);
-                      audioChunks = [];
-                      var fileReader = new FileReader();
-                      fileReader.readAsDataURL(audioBlob);
-                      fileReader.onloadend = function () {
-                          var base64String = fileReader.result;
-                          socket.emit("audioStream", base64String);
-                      };
+    //               madiaRecorder.addEventListener("stop", function () {
+    //                   var audioBlob = new Blob(audioChunks);
+    //                   audioChunks = [];
+    //                   var fileReader = new FileReader();
+    //                   fileReader.readAsDataURL(audioBlob);
+    //                   fileReader.onloadend = function () {
+    //                       var base64String = fileReader.result;
+    //                       socket.emit("audioStream", base64String);
+    //                   };
           
-                      madiaRecorder.start();
-                      setTimeout(function () {
-                          madiaRecorder.stop();
-                      }, 1000);
-                  });
+    //                   madiaRecorder.start();
+    //                   setTimeout(function () {
+    //                       madiaRecorder.stop();
+    //                   }, 1000);
+    //               });
           
-                  madiaRecorder.start();
-                  setTimeout(function () {
-                      madiaRecorder.stop();
-                  }, 1000);
-              })
-              .catch((error) => {
-                  console.error('Error capturing audio.', error);
-              });
-          });
+    //               madiaRecorder.start();
+    //               setTimeout(function () {
+    //                   madiaRecorder.stop();
+    //               }, 1000);
+    //           })
+    //           .catch((error) => {
+    //               console.error('Error capturing audio.', error);
+    //           });
+    //       });
 
-          socket.on('audioStream', (audioData) => {
-            var newData = audioData.split(";");
-            console.log(newData);
-            newData[0] = "data:audio/ogg;";
-            newData = newData[0] + newData[1];
+    //       socket.on('audioStream', (audioData) => {
+    //         var newData = audioData.split(";");
+    //         console.log(newData);
+    //         newData[0] = "data:audio/ogg;";
+    //         newData = newData[0] + newData[1];
         
-            var audio = new Audio(newData);
-            console.log(audio);
-            if (!audio || document.hidden || audio.src == "data:audio/ogg;base64," ){
-                return;
-            }
+    //         var audio = new Audio(newData);
+    //         console.log(audio);
+    //         if (!audio || document.hidden || audio.src == "data:audio/ogg;base64," ){
+    //             return;
+    //         }
 
-            audio.play();
-        });
-    },[])
+    //         audio.play();
+    //     });
+    // },[])
 
   return (
 
     <> 
    
-    {/* <input type="text" id="room-id" value="abcdef" /> */}
+    {/* <input type="text" id="room-id" value="abcdef" />
     <button  onClick={openRoom} id="open-room">Open Room</button>
     <button onClick={joinRoom} id="join-room">Join Room</button>
     <button id="open-or-join-room">Auto Open Or Join Room</button>
@@ -95,6 +97,8 @@ function Page() {
       console.log("clicked" , push);
       push = !push
     }}> ads;fjak;ldjfk;lasdfkasfads';</button>
+  </> */}
+  <EmojiSelector/>
   </>
   );
 }
